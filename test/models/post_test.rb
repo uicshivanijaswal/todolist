4ab28c539eff2a399844c901d445343d4d5911e1
edit <uic.shivanijaswal@gmail.com>
2023-11-14 13:53:14 +0000

    add posts

---
+++ test/models/post_test.rb
@@ -0,0 +1,14 @@
+# frozen_string_literal: true
+
+require 'test_helper'
+
+class PostTest < ActiveSupport::TestCase
+  test "should not save a post without a title" do
+    post = Post.new
+    assert_not post.save, "Saved the post without a title"
+  end 
+  test "should save a post with a title" do
+    post = Post.new(title: "Test")
+    assert post.save, "Saved the post with a title presence"
+  end 
+end
